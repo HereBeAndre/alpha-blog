c83b9ce10bd5ea18fa919d306a79e9cdf40dfb2c
Andrea Tota <andrea.tota6@gmail.com>
2020-06-06 14:58:09 +0200

    Create edit view containing a form. Add edit and update actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,11 @@
 class ArticlesController < ApplicationController
 
+  before_action :find_article, only: [:show, :edit, :update]
   def index
     @articles = Article.all
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
+  def show; end
 
   def new
     # Send empty instance variable to form page in order for flash to work
@@ -30,10 +29,25 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = 'Article was updated successfully'
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
   end
 
 end
