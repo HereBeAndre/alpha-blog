ce1fdcc43b4a8b8930e7df77c23f0a24ebfd3cbe
Andrea Tota <andrea.tota6@gmail.com>
2020-06-06 11:17:54 +0200

    ArticlesController: add private method to permit params, add logic to create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,21 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article]
+    # Debugging hack to see what is received from the form in new.html.erb =>
+    # render plain: params[:article]
+
+    @article = Article.new(article_params)
+    # Same debugging hack as above
+    # render plain: @article.inspect
+    @article.save
+    redirect_to @article # Shortened for standard line below
+    # redirect_to article_path(@article)
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 
 end
