29e145ac6f16e7c23cf8917f56f8870787a68a98
Andrea Tota <andrea.tota6@gmail.com>
2020-06-06 17:18:00 +0200

    Create partials for new and edit form, also for flash message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,7 +34,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = 'Article was updated successfully'
+      flash[:notice] = 'Article was updated successfully.'
       redirect_to @article
     else
       render 'edit'
